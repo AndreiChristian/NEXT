1c3e038e4ed2cc1deb12a711e3306689678c0514
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-14 17:59:00 +0200

    implemented art page and art:id

--- pupper-palette/src/pages/art/[artId].tsx
+++ pupper-palette/src/pages/art/[artId].tsx
@@ -1,7 +1,89 @@
+import { query } from "@/db";
+import styles from "../../styles/pagesStyles/ArtId.module.css";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+
 import React from "react";
 
-const ArtItem = () => {
-  return <div>ArtItem</div>;
+export type DogDetail = {
+  id: number;
+  name: string;
+  description: string;
+  breed_id: number;
+  category_id: number;
+  price: number;
+  imagepath: string;
+  breed: string;
+  category: string;
+};
+
+type ArtIdProps = {
+  dog: DogDetail;
+};
+
+const ArtItem: React.FC<ArtIdProps> = ({ dog }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.product}>
+        <span className={styles.img_container} style={{ position: "relative" }}>
+          <Image src={dog.imagepath} alt={dog.name} fill></Image>
+        </span>
+        <div className={styles.details}>
+          <h1>{dog.breed}</h1>
+          <p>{dog.description}</p>
+          <h3>{dog.price}</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ArtItem;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { rows } = await query(
+    `SELECT d.*,
+  b.name as breed,
+  c.name as category
+FROM dogs as d
+  JOIN categories as c ON d.category_id = c.id
+  JOIN breeds as b ON d.breed_id = b.id`,
+    []
+  );
+
+  const paths = rows.map((row) => {
+    return {
+      params: {
+        artId: row.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true, // false or "blocking"
+  };
+};
+
+export const getStaticProps: GetStaticProps<ArtIdProps> = async ({
+  params,
+}) => {
+  const { artId } = params!; // Extract the artId from params
+
+  const { rows } = await query(
+    `SELECT d.*,
+      b.name as breed,
+      c.name as category
+    FROM dogs as d
+      JOIN categories as c ON d.category_id = c.id
+      JOIN breeds as b ON d.breed_id = b.id
+    WHERE d.id = $1`,
+    [artId] // Use artId instead of params as the parameter value
+  );
+
+  return {
+    props: {
+      dog: rows[0],
+    },
+  };
+};
--- pupper-palette/src/pages/art/index.tsx
+++ pupper-palette/src/pages/art/index.tsx
@@ -1,20 +1,79 @@
+import { query } from "@/db";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import React from "react";
+import styles from "../../styles/pagesStyles/ArtIndex.module.css";
+import { useRouter } from "next/router";
 
-const ArtIndex = () => {
+export type ArtIndexProps = {
+  dogs: DogOverview[];
+};
+
+const ArtIndex: React.FC<ArtIndexProps> = ({ dogs }) => {
+  const router = useRouter();
+
+  const onHandleClick = (id: number) => {
+    router.push(`art/${id}`);
+  };
+
   return (
-    <>
+    <div className={styles.container}>
       <h1>Art</h1>
-      <Image
-        src={
-          "https://github.com/AndreiChristian/NEXT/blob/master/pupper-palette/public/images/cubist/australian.png?raw=true"
-        }
-        alt="dog image"
-        width={500}
-        height={500}
-      />
-    </>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, unde
+        quas! Neque debitis ratione ea et vero quaerat, sapiente quia
+        necessitatibus dolorem consequatur facilis libero cumque fuga molestiae
+        saepe corrupti?
+      </p>
+      <div className={styles.grid}>
+        {dogs.map((dog) => {
+          return (
+            <div
+              key={dog.id}
+              className={styles.item}
+              onClick={() => onHandleClick(dog.id)}
+            >
+              <Image
+                src={dog.imagepath}
+                alt={dog.name}
+                // height={100}
+                // width={100}
+                style={{
+                  objectFit: "cover",
+                }}
+                fill
+              ></Image>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
 export default ArtIndex;
+
+export type DogOverview = {
+  id: number;
+  name: string;
+  description: string;
+  breed_id: number;
+  category_id: number;
+  price: number;
+  imagepath: string;
+};
+
+export const getStaticProps: GetStaticProps<ArtIndexProps> = async () => {
+  const { rows } = await query(
+    `SELECT *
+  FROM dogs
+  ORDER BY id`,
+    []
+  );
+
+  return {
+    props: {
+      dogs: rows,
+    },
+  };
+};
