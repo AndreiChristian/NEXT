f0fa2803b9a80854198a9df92308ed7160132954
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-14 16:06:26 +0200

    hello

--- pupper-palette/src/pages/art/index.tsx
+++ pupper-palette/src/pages/art/index.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const ArtIndex = () => {
   return (
     <>
       <h1>Art</h1>
+      <Image
+        src={
+          "https://github.com/AndreiChristian/NEXT/blob/master/pupper-palette/public/images/cubist/australian.png?raw=true"
+        }
+        alt="dog image"
+        width={500}
+        height={500}
+      />
     </>
   );
 };
